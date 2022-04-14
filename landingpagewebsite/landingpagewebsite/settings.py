f56b47bcb06104788d5a45a3c07f362b3a6b0581
Denys Pankratov <den4ikvaper@gmail.com>
2022-04-15 00:03:21 +0300

    15.04.2022

--- landingpagewebsite/landingpagewebsite/settings.py
+++ landingpagewebsite/landingpagewebsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,7 +120,11 @@ STATICFILES_DIRS = [
     BASE_DIR / 'landingpagewebsite/static'
 ]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
